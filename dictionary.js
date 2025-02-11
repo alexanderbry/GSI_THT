b5835ff326a26f5b17cb0a9890c59fedd5160682
alexanderbry <alexunderbrain@gmail.com>
2025-02-11 17:28:06 +0700

    refactor number 1 and dictionary

---
+++ dictionary.js
@@ -0,0 +1,14 @@
+const dictionary = [
+  ["Avwxyz ", "0"],
+  ["BCDu", "1"],
+  ["Epqrst", "2"],
+  ["FGHo", "3"],
+  ["Ijklmn", "4"],
+  ["JKLMNi", "5"],
+  ["Ofgh", "6"],
+  ["PQRSTe", "7"],
+  ["Ubcd", "8"],
+  ["VWXYZa", "9"],
+];
+
+module.exports = dictionary;
